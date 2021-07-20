00e79df74b181df60f91f2df9d9357fdf1909c1c
Sayantan Khan <sayantangkhan@gmail.com>
2021-07-19 22:53:53 -0400

    Made a proper command line program using clap

--- src/bin/prog-1.rs
+++ src/bin/prog-1.rs
@@ -1,32 +1,55 @@
+use clap::{crate_version, AppSettings, Clap};
 use julia::*;
 use num_complex::Complex64;
-use std::env;
 use std::fs::File;
 use std::io::BufWriter;
 
+/// A command line program to plot the filled Julia set of z^2 + c
+#[derive(Clap)]
+#[clap(version = crate_version!(), author = "Sayantan Khan <saykhan@umich.edu>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    /// Width of the generated image in pixels
+    #[clap(long, default_value = "4000")]
+    width: usize,
+    /// Height of the generated image in pixels
+    #[clap(long, default_value = "4000")]
+    height: usize,
+    /// Complex coordinate of the top left corner
+    #[clap(long, default_value = "-2.0 + 2.0i")]
+    top_left: Complex64,
+    /// Complex coordinate of the bottom right corner
+    #[clap(long, default_value = "2.0 - 2.0i")]
+    bottom_right: Complex64,
+    /// The value of c in the iterated polynomial z^2+c
+    #[clap(short)]
+    c: Complex64,
+    /// Maximum number of iterates for a point
+    #[clap(short, long, default_value = "100")]
+    escape_threshold: usize,
+    /// Squared radius of ball around origin
+    #[clap(short, long, default_value = "1e4")]
+    radius_squared: f64,
+    /// Output file
+    #[clap(short, long)]
+    output: String,
+}
+
 fn main() {
-    let path = env::args()
-        .nth(1)
-        .expect("Expected a filename to output to.");
-    let file = File::create(path).unwrap();
+    let opts: Opts = Opts::parse();
+
+    let file = File::create(opts.output).expect("Unable to create output file");
     let ref mut w = BufWriter::new(file);
 
-    let top_left = Complex64::new(-2.0, 2.0);
-    let bottom_right = Complex64::new(2.0, -2.0);
-    let width = 6000;
-    let height = 6000;
-    let c = Complex64::new(-0.1, 0.75);
-    let escape_threshold = 1000;
-    let radius_squared = 1e8;
     plot_basin_of_infinity(
         w,
-        width,
-        height,
-        top_left,
-        bottom_right,
-        c,
-        escape_threshold,
-        radius_squared,
+        opts.width,
+        opts.height,
+        opts.top_left,
+        opts.bottom_right,
+        opts.c,
+        opts.escape_threshold,
+        opts.radius_squared,
     )
     .unwrap();
 }
